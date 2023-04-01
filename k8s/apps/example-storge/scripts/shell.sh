1bcaaef0e3abf6dcac74d65767dabb0f51393cfe
Scott Willeke <scott@willeke.com>
2023-04-01 15:36:39 -0700

    feat: storage example showing how to read/write files on host

---
+++ k8s/apps/example-storge/scripts/shell.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+THISDIR=$(cd $(dirname "$0"); pwd) #this script's directory
+THISSCRIPT=$(basename $0)
+WORKSPACE_ROOT=`cd $THISDIR/../..; pwd` 
+RESOURCE_ROOT="$WORKSPACE_ROOT"
+
+set -x
+
+kubectl exec -it example-storage -- /bin/sh
