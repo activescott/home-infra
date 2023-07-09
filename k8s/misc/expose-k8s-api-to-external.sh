5fc09f2813b9cc981021ad27bdc5d4cbb4dbd6b5
Scott Willeke <scott@willeke.com>
2023-07-09 13:02:47 -0700

    feat: unblock truenas k8s port script

---
+++ k8s/misc/expose-k8s-api-to-external.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+set -x
+
+/sbin/iptables -D INPUT -p tcp -m tcp --dport 6443 -m comment --comment "iX Custom Rule to drop connection requests to k8s cluster from external sources" -j DROP
+
