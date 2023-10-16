588643146292ebc5abed16e9a8b396ebf1907ab3
Scott Willeke <scott@willeke.com>
2023-10-16 14:34:17 -0700

    fix: remove managedByLabel from cert-manager

--- k8s/scripts/_include.args.sh
+++ k8s/scripts/_include.args.sh
@@ -15,8 +15,16 @@ fi
 
 K8S_APP_NAME="$1"
 RESOURCE_ROOT="$APPS_ROOT/$K8S_APP_NAME"
+
 # Check if directories exist
 if [ ! -d "$RESOURCE_ROOT" ]; then
-    echo "Error: The app name '$K8S_APP_NAME' does not exist."
-    exit 1
+    echo "$RESOURCE_ROOT didn't exist..."
+    # it didn't exist as is, but check if it really just wanted the basename (e.g. user typed `./k8s/scripts/preview.sh ./k8s/apps/cert-manager`):
+    RESOURCE_ROOT="$APPS_ROOT/`basename $K8S_APP_NAME`"
+    echo "trying $RESOURCE_ROOT..."
+    if [ ! -d "$RESOURCE_ROOT" ]; then
+        # still invalid, so error out
+        echo "Error: The app name '$K8S_APP_NAME' does not exist."
+        exit 1
+    fi   
 fi
