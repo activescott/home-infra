16c5ad86954ed9db2a4e0cf692096bf159a394af
Scott Willeke <scott@willeke.com>
2024-01-30 21:51:11 -0800

    fix: add lodestars consensus rpc port to service

---
+++ k8s/apps/eth/scripts/lodestar-node-syncing.sh
@@ -0,0 +1,7 @@
+#/bin/env bash 
+
+# standard beacon/consensus rpc api: https://ethereum.github.io/beacon-APIs/#/
+curl http://k8s.activescott.com:9596/eth/v1/node/syncing \
+  -X GET \
+  -H "Content-Type: application/json" \
+    | jq
